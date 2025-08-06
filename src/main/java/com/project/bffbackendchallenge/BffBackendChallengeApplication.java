2b15c07c50d9e1381fb50744a7e1c634817a9848
dcanchari <dtcancharir@gmail.com>
2025-08-05 23:31:26 -0500

    Init commit

---
+++ src/main/java/com/project/bffbackendchallenge/BffBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.project.bffbackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffBackendChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffBackendChallengeApplication.class, args);
+    }
+
+}
